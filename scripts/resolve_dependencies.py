b354e84abfdf808272012db8bf79788d0c1cabe7
lens0021 <lorentz0021@gmail.com>
2021-02-14 14:48:20 +0900

    Exclude EventBus tests in REL1_35
    Related with https://github.com/femiwiki/.github/issues/4

--- scripts/resolve_dependencies.py
+++ scripts/resolve_dependencies.py
@@ -2,6 +2,11 @@
 
 # pf for https://raw.githubusercontent.com/wikimedia/integration-config/master/zuul/parameter_functions.py
 from pf import dependencies, get_dependencies
+from os import environ
+
+# https://github.com/femiwiki/.github/issues/4
+if 'MEDIAWIKI_VERSION' in environ and environ['MEDIAWIKI_VERSION'] == 'REL1_35':
+  dependencies['EventLogging'].remove('EventBus')
 
 dependencies['ext'] = open('dependencies').read().splitlines()
 print(' '.join(['mediawiki/extensions/' + d for d in get_dependencies('ext', dependencies)]))
