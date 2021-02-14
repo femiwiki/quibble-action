420faa4ecf44ac59955a5f51a541c9c88e2d25ba
lens0021 <lorentz0021@gmail.com>
2021-02-14 15:24:39 +0900

    Resolve skin dependencies

--- scripts/resolve_dependencies.py
+++ scripts/resolve_dependencies.py
@@ -1,12 +1,20 @@
 # A script to resolve dependencies of MediaWiki extension for Quibble test
 
+from os import environ
 # pf for https://raw.githubusercontent.com/wikimedia/integration-config/master/zuul/parameter_functions.py
 from pf import dependencies, get_dependencies
-from os import environ
 
 # https://github.com/femiwiki/.github/issues/4
 if 'MEDIAWIKI_VERSION' in environ and environ['MEDIAWIKI_VERSION'] == 'REL1_35':
   dependencies['EventLogging'].remove('EventBus')
 
+# Add dependencies of target extension
 dependencies['ext'] = open('dependencies').read().splitlines()
-print(' '.join(['mediawiki/extensions/' + d for d in get_dependencies('ext', dependencies)]))
+
+# Resolve
+resolvedDependencies = []
+for d in get_dependencies('ext', dependencies):
+  d = 'mediawiki/extensions/' + d
+  d = d.replace('/extensions/skins/', '/skins/')
+  resolvedDependencies.append(d)
+print(' '.join(resolvedDependencies))
